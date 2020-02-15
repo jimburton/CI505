ca7757ef2be9a4a89148b71fb7247e96ea58d74e
Jim Burton <j.burton@brighton.ac.uk>
2020-02-15 21:11:32 +0000

    Refactored Huffman

--- labs/huffman/Huffman.hs
+++ labs/huffman/Huffman.hs
@@ -15,23 +15,23 @@ an element and the second element is its frequency
 type FreqTable a = [(a, Int)]
 
 {-
-Types to represent the Huffman encoding.
--}
-data PathPart a = L | R | E a            deriving Show
-type Path a     = [PathPart a]
-type HCode a    = [(a, Path a)]
-type HEncoded a = (Path a, HTree a)
-
-{- A data type of binary trees, HTree a, where each node
+A data type of binary trees, HTree a, where each node
 carries an int (representing frequency counts) and and leaves carry an
 additional piece of data of type a (e.g. a char). A HTree is a Huffman
 Tree. In a leaf node, (Leaf i c), i will represent the frequency of
 occurence of c. In a branch node (Branch i l r), i will represent the
 combined frquencies of the left and right children.
-
-Your data type should derive the `Show` and `Eq` type classes. -}
+ -}
 
 data HTree a = Leaf Int a | Branch Int (HTree a) (HTree a) deriving (Show, Eq)
+
+{-
+Types to represent the Huffman encoded data.
+-}
+data PathPart a = L | R | E a            deriving Show
+type Path a     = [PathPart a]
+type HCode a    = [(a, Path a)]
+type HEncoded a = (Path a, HTree a)
 
 {-
 1. Complete the `ftable` function which constructs the sorted
@@ -46,6 +46,13 @@ fTable = sort' . freq
                     freqInner (d,n) (e:es) = let m = if d==e then n+1 else n
                                              in freqInner (d,m) es
 
+-- Pseudo-QuickSort 
+sort' :: Ord b => [(a,b)] -> [(a,b)]
+sort' [] = []
+sort' (x:xs) = sort' lt ++ [x] ++ sort' gt
+    where lt = filter (\(y,z) -> z < snd x) xs
+          gt = filter (\(y,z) -> z >= snd x) xs
+
 {-
 2. Complete the `insert` function, which inserts a `HTree` node into a
    list sorted by ascending frequency.
@@ -56,12 +63,15 @@ insert h [] = [h]
 insert h (t:ts) = if getFreq h < getFreq t then h:t:ts
                   else t : insert h ts
 
+getFreq (Branch i _ _) = i
+getFreq (Leaf i _)     = i
+
 {-
 3. Merge a list of `HTree` nodes into a single `Maybe HTree`. If the input
 is empty, return `Nothing`. If the input contains a single element, we
 are done. Otherwise, do the following: 
     * create a `Branch` node from the first two elements in the input,
-	* use your `insert` function (see below) to insert this new
+	* use your `insert` function to insert this new
       element in the right place in the new list, which is formed of the old 
       list without its first two elements,
     * call `merge` recursively on the new list.
@@ -72,8 +82,8 @@ will be the left-hand child in the new `Branch` node.
 -}
 
 merge :: [HTree a] -> Maybe (HTree a)
-merge [] = Nothing
-merge (x:[]) = Just x
+merge []       = Nothing
+merge [x]      = Just x
 merge (x:y:zs) = let cX = getFreq x
                      cY = getFreq y
                      i = cX + cY in
@@ -89,7 +99,7 @@ the following:
     * construct the frequency table for `str`,
 	* use the frequency table to create a list of Leaf nodes, 
 	* merge that list into a single tree by calling your `merge`
-      function (which is the next problem).
+      function.
 
 -}
 
@@ -108,22 +118,21 @@ tree str = if not $ null str
 
 generateCode :: Ord a => HTree a -> HCode a
 generateCode (Leaf i c)     = []
-generateCode (Branch i l r) = map toLookup $ (pathsInner L l) ++ (pathsInner R r)
-  where pathsInner dir (Leaf _ c)     = [[dir, E c]]
-        pathsInner dir (Branch _ l r) = (map (dir :) (pathsInner L l)) ++ (map (dir :) (pathsInner R r))
+generateCode (Branch i l r) = map toLookup $ paths L l ++ paths R r
+  where paths dir (Leaf _ c)     = [[dir, E c]]
+        paths dir (Branch _ l r) = map (dir :) (paths L l) ++ map (dir :) (paths R r)
         toLookup ps                   = let (E c) = last ps in (c, init ps)
 
 {-
 6. Complete the `encode` function, which creates the tree for the
    input, `str`, generates the code for that tree, and uses it to
-   encode `str`. It reutns a pair of the encoded input and the HCode
+   encode `str`. It returns a pair of the encoded input and the HCode
 used to encode it.
 -}
 
 encode :: Ord a => [a] -> Maybe (HEncoded a)
-encode str = case tree str of
-  Nothing -> Nothing
-  Just t  -> let code = generateCode t in Just (concatMap (fromJust . (\c -> lookup c code)) str, t)
+encode str = tree str >>= \t -> let code = generateCode t in
+  return (concat $ concat <$> sequenceA $ map (`lookup` code) str, t)
 
 
 {-
@@ -131,7 +140,7 @@ encode str = case tree str of
    a `HCode` object and returns the decoded result.
 -}
 
---decode :: Ord a => Path a -> HTree a -> [a]
+decode :: HTree a -> Path a -> [a]
 decode t pps = decodeInner t pps
   where decodeInner (Leaf _ c) []          = [c]
         decodeInner (Leaf _ c) input       = c : decodeInner t input
@@ -139,30 +148,16 @@ decode t pps = decodeInner t pps
         decodeInner (Branch _ _ r) (R:ps)  = decodeInner r ps
         decodeInner _ []                   = []
 
-------------
--- Helper functions
-------------
-
--- A getter for the frequency count in a HTree node.
-getFreq :: HTree a -> Int
-getFreq (Branch i _ _) = i
-getFreq (Leaf i _) = i
-
--- Pseudo-QuickSort 
-sort' :: Ord b => [(a,b)] -> [(a,b)]
-sort' [] = []
-sort' (x:xs) = (sort' lt) ++ [x] ++ (sort' gt)
-    where lt = filter (\(y,z) -> z < snd x) xs
-          gt = filter (\(y,z) -> z >= snd x) xs
-
+------------------
 -- Drawing trees
+------------------
 
 toDataTree :: Show a => HTree a -> Data.Tree.Tree String
 toDataTree (Leaf i c) = Node (showLeafData c i) []
 toDataTree (Branch i l r) = Node (show i) [toDataTree l, toDataTree r]
 
 showLeafData :: (Show a, Show b) => a -> b -> String
-showLeafData x y = (show y)++":"++(show x)
+showLeafData x y = show y ++ ":" ++ show x
 
 printMaybeTree :: Show a => Maybe (HTree a) -> IO ()
 printMaybeTree Nothing  = putStrLn ""
---
+++ labs/parsing/Log.hs
@@ -0,0 +1,76 @@
+-- This file accompanies CI285 Exercise 7
+
+module Log where
+
+-- | The classification of a message
+data MessageType = Info
+                 | Warning
+                 | Error Int    -- ^ The parameter is the severity of the error
+  deriving (Show, Eq)
+   -- this line allows equality comparison and printing in GHCi
+
+type TimeStamp = Int
+
+data LogMessage = LogMessage MessageType TimeStamp String
+  deriving (Show, Eq)
+
+data MaybeLogMessage = ValidLM LogMessage
+                     | InvalidLM String
+  deriving (Show, Eq)
+
+data MaybeInt = ValidInt Int
+              | InvalidInt
+  deriving (Show, Eq)
+
+-- Test functions
+
+testReadLogFile :: (String -> IO [String]) -> String -> IO ()
+testReadLogFile readF str = do ls <- readF str
+                               mapM_ putStrLn ls
+
+testParseMessage :: (String -> IO [String]) ->
+                    String ->
+                    (String -> MaybeLogMessage) ->
+                    IO ()
+testParseMessage readF path parseF = do ls <- readF path
+                                        mapM_ (putStrLn . show . parseF) ls
+
+testParse :: (String -> IO [LogMessage]) ->
+             Int ->
+             String ->
+             IO ()
+testParse parseF i path = do ls <- parseF path
+                             mapM_ (putStrLn . show) (take i ls)
+
+testWhatWentWrong :: (String -> IO [LogMessage]) ->
+                     String ->
+                    ([LogMessage] -> [String]) ->
+                     IO ()
+testWhatWentWrong parseF path wwwF = do ls <- parseF path
+                                        let strs = wwwF ls 
+                                        print strs
+
+{- Monadic versions for comparison -}
+testReadLogFile' :: (String -> IO [String]) -> String -> IO ()
+testReadLogFile' readF str = readF str >>= mapM_ putStrLn
+
+testParseMessage' :: (String -> IO [String]) ->
+                    String ->
+                    (String -> MaybeLogMessage) ->
+                    IO ()
+testParseMessage' readF path parseF = readF path >>=
+                                      mapM_ (putStrLn . show . parseF) 
+
+testParse' :: (String -> IO [LogMessage]) ->
+             Int ->
+             String ->
+             IO ()
+testParse' parseF i path = parseF path >>=
+                           mapM_ (putStrLn . show) . (take i)
+
+testWhatWentWrong' :: (String -> IO [LogMessage]) ->
+                     String ->
+                    ([LogMessage] -> [(TimeStamp, String)]) ->
+                     IO ()
+testWhatWentWrong' parseF path wwwF = parseF path >>= (return . wwwF) >>= print
+                           
