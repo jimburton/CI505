94a4eb18a7fddeb68d7f2d1dbe6f7b79353f3d55
James Burton <j.burton@brighton.ac.uk>
2021-02-11 16:26:46 +0000

    Week 2 exercises

--- labs/Exercises.hs
+++ labs/Exercises.hs
@@ -27,3 +27,28 @@ take' :: Int -> [a] -> [a]
 take' 0 xs     = []
 take' n []     = []
 take' n (x:xs) = x : take (n-1) xs
+
+--------------------------
+-- Week 2
+--------------------------
+
+pack :: Eq a => [a] -> [[a]]
+pack []     = []
+pack (x:xs) = (x : takeWhile (==x) xs) : pack (dropWhile (==x) xs)
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = map (\ys -> (length ys, head ys)) (pack xs)
+
+decode :: Eq a => [(Int, a)] -> [a]
+decode xs = concatMap (\(i, c) -> replicate i c) xs
+
+data RLE a = Single a | Multiple Int a deriving Show
+
+encodeRLE :: Eq a => [a] -> [RLE a]
+encodeRLE xs = map (\(i, c) -> if i == 1 then Single c
+                     else Multiple i c) (encode xs)
+
+decodeRLE :: Eq a => [RLE a] -> [a]
+decodeRLE xs = concatMap (\r -> case r of
+                             (Single c)     -> [c]
+                             (Multiple i c) -> replicate i c) xs
